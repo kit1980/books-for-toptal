7ee746b7bd8dd2b4470bddcf302beddf4e43b18f
Sergey Dymchenko <kit1980@gmail.com>
2010-12-05 23:55:19 +0200

    Added logout.

--- books_for_toptal_prj/book_reviews/views.py
+++ books_for_toptal_prj/book_reviews/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
+
 
 from book_reviews.models import Review, Category
 
@@ -6,3 +9,7 @@ def home(request):
     return direct_to_template(
         request,
         template='book_reviews/home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('book_reviews.views.home')
--- books_for_toptal_prj/urls.py
+++ books_for_toptal_prj/urls.py
@@ -6,6 +6,9 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     (r'^rpx-response/$', 'django_rpx.views.rpx_response'),
+    (r'^logout/$', 'book_reviews.views.logout_view'),
+
+
     (r'^/?$', 'book_reviews.views.home'),
 # (r'^category/([\w]+)/$', 'book_reviews.views.category_detail'),
 # (r'^review/([\d]+)/$', 'book_reviews.views.review_detail'),
