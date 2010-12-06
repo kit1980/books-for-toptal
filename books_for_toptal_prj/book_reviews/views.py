5c2e97b9eb02f61f08bb6f55356544961986b8fe
Sergey Dymchenko <kit1980@gmail.com>
2010-12-06 02:05:02 +0200

    Added category views.

--- books_for_toptal_prj/book_reviews/views.py
+++ books_for_toptal_prj/book_reviews/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import list_detail
 from django.views.generic.simple import direct_to_template
 
 from book_reviews.models import Review, Category
 
 def home(request):
-    return direct_to_template(
+    return list_detail.object_list(
         request,
-        template='book_reviews/home.html')
+        queryset=Review.objects.all(),
+        extra_context={'sorting': 'top'})
+
+def home_new(request):
+    return list_detail.object_list(
+        request,
+        queryset=Review.objects.all(),
+        extra_context={'sorting': 'new'})
 
 def logout_view(request):
     logout(request)
@@ -20,3 +27,13 @@ def review_detail(request, review_id):
         queryset = Review.objects.all(),
         object_id=review_id,
     )
+
+def review_list(request, category_slug, sorting):
+    category = get_object_or_404(Category, slug=category_slug)
+    if sorting != "new":
+        sorting = "top"
+
+    return list_detail.object_list(
+        request,
+        queryset=category.review_set.all(),
+        extra_context={'category': category, 'sorting': sorting})
--- books_for_toptal_prj/urls.py
+++ books_for_toptal_prj/urls.py
@@ -8,10 +8,11 @@ urlpatterns = patterns(
     (r'^rpx-response/$', 'django_rpx.views.rpx_response'),
     (r'^logout/$', 'book_reviews.views.logout_view'),
 
-
     (r'^/?$', 'book_reviews.views.home'),
-# (r'^category/([\w]+)/$', 'book_reviews.views.category_detail'),
+    (r'^new/?$', 'book_reviews.views.home_new'),
+
     (r'^review/([\d]+)/$', 'book_reviews.views.review_detail'),
+    (r'^category/([-\w]+)/(top|new)/$', 'book_reviews.views.review_list'),
 
 
     (r'^admin/', include(admin.site.urls)),
